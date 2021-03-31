1832bbed2380a075d40ff6ab4dc5c15d0b3b21ca
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-03-31 09:46:48 -0300

    feat(nodejs/chapter1): criando saque na conta (completo)

--- nodejs/aulas/chapter-1/first-project/README.md
+++ nodejs/aulas/chapter-1/first-project/README.md
@@ -4,8 +4,13 @@
 
 - [x] Deve ser possível criar uma conta
 - [x] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível realizar um depósito
+- [] Deve realizar um saque
 
 ## Regras de negócio
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
 - [x] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível fazer um depósito em uma conta não existente
+- [] Não deve ser possível fazer um saque em uma conta não existente
+- [] Não deve ser possível fazer um saque quando o saldo da conta for insuficiente
--- nodejs/aulas/chapter-1/first-project/src/index.js
+++ nodejs/aulas/chapter-1/first-project/src/index.js
@@ -24,6 +24,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -51,6 +63,44 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds" });
+  }
+
+  const statementOperation = {
+    description: 'withdraw',
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(port);
