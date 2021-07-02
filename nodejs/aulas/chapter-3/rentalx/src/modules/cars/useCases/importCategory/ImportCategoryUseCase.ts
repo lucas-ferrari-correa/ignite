ccae69bf4dfc7bae7676db10b47f14d34847d081
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-07-01 21:24:04 -0300

    feat(nodejs/chapter3): injeçao de dependencia (completo)

--- nodejs/aulas/chapter-3/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ nodejs/aulas/chapter-3/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,14 +1,20 @@
 import csvParse from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from "tsyringe";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 
 interface IImportCategory {
   name: string;
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository,
+  ) {}
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
@@ -43,10 +49,10 @@ class ImportCategoryUseCase {
     categories.map(async category => {
       const { name, description } = category;
 
-      const existCategory = this.categoriesRepository.findByName(name);
+      const existCategory = await this.categoriesRepository.findByName(name);
 
       if (!existCategory) {
-        this.categoriesRepository.create({
+        await this.categoriesRepository.create({
           name,
           description,
         });
