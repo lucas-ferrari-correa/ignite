bfa00dbbe762126951c85a1206b6d056c1c5dfa5
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-09-01 00:25:37 -0300

    feat(nodejs/chapter4): capitulo 4 completo

--- nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
-import { AppError } from "@shared/errors/AppError";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
   sub: string;
@@ -19,8 +19,6 @@ export async function ensureAuthenticated(
   }
 
   const [, token] = authHeader.split(" ");
-
-  console.log("olar");
 
   try {
     const { sub: userId } = verify(
