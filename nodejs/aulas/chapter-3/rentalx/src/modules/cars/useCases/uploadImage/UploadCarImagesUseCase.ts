e01f7172818ab722877b553c533b8f2931f899eb
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-11-25 22:52:17 -0300

    feat(nodejs/chapter6): upload utilizando S3 completo

--- nodejs/aulas/chapter-3/rentalx/src/modules/cars/useCases/uploadImage/UploadCarImagesUseCase.ts
+++ nodejs/aulas/chapter-3/rentalx/src/modules/cars/useCases/uploadImage/UploadCarImagesUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { ICarsImagesRepository } from "@modules/cars/repositories/in-memory/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,11 +12,14 @@ class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
     private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async image => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
