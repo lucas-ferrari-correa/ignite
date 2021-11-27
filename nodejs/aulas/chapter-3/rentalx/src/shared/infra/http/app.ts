b6772e8bfd7bb26dff68701ba1a45d0ca9306d65
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-11-27 00:20:27 -0300

    feat(nodejs/chapter6): upload url avatar completo

--- nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/app.ts
+++ nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 import swaggerFile from "../../../swagger.json";
@@ -15,6 +16,8 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
