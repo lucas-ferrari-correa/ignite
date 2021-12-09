ef3e058dce9fa9b5b140627f304817e2b8543857
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-12-08 21:10:55 -0300

    feat(nodejs/chapter6): ratelimiter

--- nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/app.ts
+++ nodejs/aulas/chapter-3/rentalx/src/shared/infra/http/app.ts
@@ -9,12 +9,16 @@ import "@shared/container";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
--- nodejs/aulas/chapter-3/rentalx/src/shared/infra/typeorm/index.ts
+++ nodejs/aulas/chapter-3/rentalx/src/shared/infra/typeorm/index.ts
@@ -3,6 +3,8 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (host = "database_ignite"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  console.log("database options", process.env.NODE_ENV);
+
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === "test" ? "localhost" : host,
