6e1dbc2c06aa169cc6ac7fd81b217218ff4a2f66
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2022-02-07 22:02:04 -0300

    feat(reactnative/chapter2): criaçao dos cartoes de destaque completo

--- reactnative/aulas/chapter-2/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/aulas/chapter-2/gofinances/src/screens/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { HighlightCard } from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/59514023?v=4' }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name='power'  />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCard />
     </Container>
   )
 }
