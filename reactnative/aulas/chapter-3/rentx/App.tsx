fb44f8d6d97fa882e86463897a38e168e5d74023
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2022-07-18 21:26:10 -0300

    feat(reactnative/chapter3): tema Global da Aplicação completo

---
+++ reactnative/aulas/chapter-3/rentx/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
+
+export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
